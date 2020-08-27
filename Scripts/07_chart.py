241a6a5199d4347e16f2ace09f3401d02c8ffe57
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2020-08-26 19:39:31 -0500

    Añadir Chart

---
+++ Scripts/07_chart.py
@@ -0,0 +1,19 @@
+from openpyxl.chart import PieChart, Reference
+from funciones_excel import cargar_archivo, obtener_hoja
+
+ruta = '../Data/test.xlsx'
+wb = cargar_archivo( ruta )
+hoja = obtener_hoja(wb, 'Seguidores')
+
+grafico = PieChart()
+
+categorias = Reference(hoja, min_col=2, min_row=2, max_row=hoja.max_row)
+datos = Reference(hoja, min_col=3, min_row=2, max_row=hoja.max_row)
+
+grafico.add_data( datos )
+grafico.set_categories( categorias )
+grafico.title = 'Seguidores'
+
+hoja.add_chart( grafico, 'E2' )
+
+wb.save( ruta )
