eb3fcc55e3152a2fbf3964e6384f3e49f7f30a0a
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2020-08-26 21:22:32 -0500

    Estilos en la cabecera.

--- Reportes/Scripts/app.py
+++ Reportes/Scripts/app.py
@@ -1,8 +1,6 @@
 import openpyxl
-from openpyxl.styles import Border, Side, PatternFill
 # Esto es para acceder a la fecha y hora del computador
 from datetime import datetime
-
 from report_excel import *
 
 now = datetime.now()
@@ -26,15 +24,7 @@ agregar_imagen( sheet )
 ws = wb.active
 ws.oddHeader.left.color = 'CC3366'
 
-thin = Side(border_style='thin', color='0089bb')
-titulo_border = Border(top=thin, bottom=thin, left=thin, right=thin)
-titulo_rect = PatternFill('solid', fgColor='0089bb')
-
-ws.merge_cells( 'A5:N5' )
-top_left_cell = ws['A5']
-top_left_cell.value = 'Reporte de los Países'
-top_left_cell.border = titulo_border
-top_left_cell.fill = titulo_rect
+agregar_titulo( ws )
 
 wb.save(nombre_archivo)
 
--- Reportes/Scripts/report_excel.py
+++ Reportes/Scripts/report_excel.py
@@ -1,5 +1,24 @@
 import openpyxl
+from openpyxl.styles import Border, Side, PatternFill, Font, Alignment
+
+thin = Side(border_style='thin', color='0089bb')
+titulo_border = Border(top=thin, bottom=thin, left=thin, right=thin)
+titulo_rect = PatternFill('solid', fgColor='0089bb')
+titulo_font_big = Font(name='Calibri', size=16, b=True, color='ffffff')
+titulo_alignment = Alignment(horizontal='center', vertical='center')
 
 def agregar_imagen( sheet ):
     img = openpyxl.drawing.image.Image('../Data/image/see_webinar.png')
     sheet.add_image( img, 'A1' )
+
+def agregar_titulo( ws ):
+    ws.merge_cells( 'A5:N5' )
+    top_left_cell = ws['A5']
+    top_left_cell.value = 'Reporte de los Países'
+    
+    top_left_cell.border = titulo_border
+    top_left_cell.fill = titulo_rect
+    top_left_cell.font = titulo_font_big
+    top_left_cell.alignment = titulo_alignment
+    
+
