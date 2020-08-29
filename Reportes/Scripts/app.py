68510fc07017fbfc257f1f16250f72288e65fdf0
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2020-08-28 20:24:50 -0500

    Agregar data al excel.

--- Reportes/Scripts/app.py
+++ Reportes/Scripts/app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from report_excel import *
 from data import *
 
+from openpyxl.styles import numbers
 import pandas as pd
 
 now = datetime.now()
@@ -47,14 +48,31 @@ sheet.insert_rows( inicial, final )
 
 for index_row, data_row in df_paises.iterrows():
     index_col = 1
+    data = list( data_row )
     
     sheet.cell( row=inicial, column=index_col, value=data_row['Country'] )
     index_col += 1
     sheet.cell( row=inicial, column=index_col, value=data_row['anio'] )
     index_col += 1
     
-    
-    
+    cabecera_index = 2
+    while( cabecera_index < len( cabecera ) ):
+        loc_country = df_transacciones['Country'] == data_row['Country']
+        loc_anio = df_transacciones['anio'] == data_row['anio']
+        loc_mes = df_transacciones['mes'] == int(cabecera[ cabecera_index ])
+        
+        ventas_x_mes = df_transacciones.loc[ loc_country & loc_anio & loc_mes ]
+        
+        cell = None
+        if len(ventas_x_mes) > 0:
+            cell = sheet.cell( row=inicial, column=index_col, value=ventas_x_mes['venta_usd'].values[0] )
+        else:
+            cell = sheet.cell( row=inicial, column=index_col, value='' )
+        
+        cell.number_format = numbers.FORMAT_CURRENCY_USD
+        cabecera_index += 1
+        index_col += 1
+        
     inicial += 1
 
 wb.save(nombre_archivo)
